45b67c9de3360cf397daf2141c21a47d24ff8fbe
myatnoe <khingsunthwe@gmail.com>
2023-04-07 13:40:57 +0630

    update codes

---
+++ app/Views/home.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Single Page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="<?php echo base_url('css/style.css'); ?>">
+    </head>
+    <body>
+        
+        <section id="first_section">
+            <img src="<?php echo base_url('images/IMAGE.png'); ?>" alt="" id="banner_logo" class="img-fluid">
+            <nav class="navbar navbar-expand">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                    <img src="<?php echo base_url('images/LOGO.png'); ?>"  alt="Logo">
+                    </a>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                  PRODUCTS
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">ABOUT US</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">CAREER</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            
+            <div class="mt-5 mb-4">
+                <div class="container-sm">
+                    <div class="mb-4">
+                    <img src="<?php echo base_url('images/LOGO1.png'); ?>"  alt="">
+                    </div>
+                    <p style="font-weight: bold; color: #fff; font-size: 18px">
+                        Building on the success of IronPDF for.NET
+                    </p>
+                    <div id="banner_text" style="line-height: 2.5em;">
+                        <p style="font-weight: bold; color: #fff; font-size: 50px">
+                            <span>Beta Software Program</span>
+                        </p>
+                        <p style="font-weight: bold; color: #fff; font-size: 50px">
+                            <span style="color: #c6aacc;">IronPDF for C++</span>
+                        </p>
+                    </div>
+                    <p style="font-size: 14px;color: #8F618F;" class="mt-4">
+                        Coming soon
+                    </p>
+                </div>
+            </div>
+
+            <div class="pt-5 pb-5" id="fs_second_div">
+                <div class="container-sm">
+                    <div style="line-height: 1.5em;">
+                        <p style="font-size: 40px; color: #ffffff; font-weight: bold;">
+                            Be one of the first
+                        </p>
+                        <p style="font-size: 18px; color: #ffffff; font-weight: bold;">
+                            Sign up NOW to get early access!
+                        </p>
+                    </div>
+                    <div class="input-group mb-3 rounded-pill" style="background-color: #fff; max-width: 400px;">
+                        <input type="text" class="form-control m-1 rounded-pill" placeholder="Enter email address" aria-label="Enter email address" aria-describedby="button-addon2" style="border: none;">
+                        <button class="btn btn-outline-secondary btn-lg rounded-pill m-1" type="button" id="button-addon2"><span style="padding: 10px 5px 10px 5px;">Sign up now</span></button>
+                    </div>
+                    <p>
+                        <span class="badge rounded-pill bg-light" style="color: #8F618F"># Coming soon</span> <span class="text-white" style="font-size: 14px;">IronPDF Beta Program also coming soon for <b>Python</b> | <b>Node.JS</b> | <b>Java</b></span>
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section id="second_section">
+            <div class="container-sm ">
+                <div id="ss_first_div" class="pt-5 pb-5">
+                    <div class="row mb-5 text-center" style="position: relative;">
+                    <img src="<?php echo base_url('images/badge.png'); ?>" alt="" class="img-fluid" id="badge_icon">
+                        <div class="col-md-6 offset-md-3 text-white" style="font-size: 40px; font-weight: bold;">IronPDF for C++</div>
+                    </div>
+
+        
+                    <div class="row justify-content-between text-white">
+                        <div class="col-md-4 text-md-start text-center">
+                            # General PDFs from HTML in C++
+                        </div>
+                        <div class="col-md-4 text-center">
+                            # Combine, split, and modify PDFs quickly in C++
+                        </div>
+                        <div class="col-md-4 text-md-end text-center">
+                            # Extract text and images from PDFs using C++
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="ss_second_div">
+                <div class="container-sm pt-5 pb-5">
+                    <div class="row">
+                        <div class="col-md-12 text-white">
+                            <p style="font-size: 16px; text-align: justify;">
+                                The new IronPDF library for C++ will enhance the C++ developer's tool set with robust PDF generation and conversion capabilities. Developers will be able to generate new PDFs from HTML content (from text of from URl), as well as to combine, split, extract, and modify content form existing PDFs.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 text-white">
+                            <p style="font-size: 16px; text-align: justify;">
+                                IronPDF for C++ will help developers create C++ applications that can do all of these PDF processing tasks and more, with speed, precision, control, and excellence.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section id="third_section">
+            <div class="container-sm ">
+                <div id="ts_first_div" class="pt-5 pb-5">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-12 col-sm-12 text-center" id="ts_img">
+                        <img src="<?php echo base_url('images/HTML to PDF icon.png'); ?>"  alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-9 col-md-12 col-sm-12">
+                            <div class="row">
+                                <div class="col-md-12 text-white">
+                                    <p style="font-size: 40px; font-weight: bold;">
+                                        Why make a <span style="color: #c6aacc;">C++ PDF Library</span>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-white">
+                                    <p style="font-size: 16px; text-align: justify;">
+                                        C++ is one of the most popular, oldest, and important programming languages in use, being the language of choice in low-level systems and network programming and other domains where performance is critical.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-white">
+                                    <p style="font-size: 16px; text-align: justify;">
+                                        The release of IronPDF for C++ will aid developers in building performant applications that can carry out PDF-related processing tasks.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="fourth_section">
+            <div class="container-sm ">
+                <div id="fos_first_div" class="pt-5 pb-5">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-12 text-white">
+                                    <p style="font-size: 40px; font-weight: bold;">
+                                        Early Access to <span style="color: #c6aacc;">C++ PDF Library</span>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-white">
+                                    <p style="font-size: 16px; text-align: justify;">
+                                        Joining the early access program will allow you to collaborate closely with our engineering team. You will be playing a key role in the development process as you share your early experiences using the C++ PDF library before its official launch. Your continual feedback after we release the library will be immensely helpful as we release new features and improve on existing features.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="row align-items-center justify-content-between mt-3">
+                                <div class="col-lg-4">
+                                    <div class="fos_card text-center">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <span class="badge rounded-pill" style="color: #331433; background-color:#c6aacc; padding: 10px 20px 10px 20px; vertical-align: sub;"># Released</span>
+                                            </div>
+                                            <div class="col-6">
+                                            <img src="<?php echo base_url('images/LOGO2.png'); ?>"  alt="" class="img-fluid">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="fos_card text-center">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <span class="badge rounded-pill" style="color: #c6aacc; background-color:#331433; padding: 10px 20px 10px 20px; vertical-align: sub;"># Coming soon</span>
+                                            </div>
+                                            <div class="col-6">
+                                            <img src="<?php echo base_url('images/LOGO3.png'); ?>"  alt="" class="img-fluid">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="fos_card text-center">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <span class="badge rounded-pill" style="color: #c6aacc; background-color:#331433; padding: 10px 20px 10px 20px; vertical-align: sub;"># Coming soon</span>
+                                            </div>
+                                            <div class="col-6">
+                                            <img src="<?php echo base_url('images/LOGO4.png'); ?>"  alt="" class="img-fluid">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="fos_second_div">
+                <div class="container-sm pt-5 pb-5">
+                    <div class="row mb-2 text-center">
+                        <p class="col-md-6 offset-md-3 text-white" style="font-size: 30px; font-weight: bold;">
+                            Sign up to our <span style="color: #c6aacc;">Beta Program</span>
+                        </p>
+                    </div>
+                    <div class="input-group mb-3 rounded-pill" style="background-color: #fff; max-width: 600px; margin: auto;">
+                        <input type="text" class="form-control m-1 rounded-pill" placeholder="Enter email address" aria-label="Enter email address" aria-describedby="button-addon2" style="border: none;">
+                        <button class="btn btn-outline-secondary btn-lg rounded-pill m-1" type="button" id="button-addon2"><span style="padding: 10px 5px 10px 5px;">Sign up now</span></button>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+</html>
